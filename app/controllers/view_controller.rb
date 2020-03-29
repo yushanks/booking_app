d82b26329a72b4d42424398793e52a60964bbb70
Yushiro Ishii <jis_ushan@yahoo.com>
2020-03-29 23:44:00 +0900

    view contorler genreate and helper method post keyword search

---
+++ app/controllers/view_controller.rb
@@ -0,0 +1,5 @@
+class ViewController < ApplicationController
+  def form_tag
+    @book = Book.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 第4章
+  get 'view/keyword'
+  post 'keyword/search'
 end
