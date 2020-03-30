e4fff9856c96d75154db4814d3129c761262478c
Yushiro Ishii <jis_ushan@yahoo.com>
2020-03-30 14:27:19 +0900

    select tag view

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -2,4 +2,42 @@ class ViewController < ApplicationController
   def form_tag
     @book = Book.new
   end
+
+  def form_for
+    @book = Book.new
+    #@book = Book.find(1)
+  end
+
+  def field
+    @book = Book.new
+  end
+
+  def html5
+    @book = Book.new
+  end
+
+  def select
+    @book = Book.new(publish: '技術評論社')
+  end
+  
+  def col_select
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def col_select2
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def group_select
+    @review = Review.new
+    @authors = Author.all
+  end
+
+  def group_select2
+    @review = Review.new
+    @authors = Author.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,18 @@ Rails.application.routes.draw do
   # 第4章
   get 'view/keyword'
   post 'keyword/search'
+
+  get 'view/form_tag'
+  post 'view/create'
+  get 'view/form_for'
+  get 'view/field'
+  get 'view/html5'
+  get 'view/select'
+  get 'view/col_select'
+  get 'view/group_select'
+  get 'view/select_tag'
+  get 'view/select_tag2'
+  get 'view/col_select2'
+  get 'view/group_select2'
+
 end
